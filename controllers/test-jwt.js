668020635de622d940d71720816a3305a03bb907
siseldyke <siseldyke@gmail.com>
2024-11-18 10:43:13 -0500

    verify token working

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -15,4 +15,16 @@ router.get('/sign-token', (req, res) => {
     // Send the token back to the client
     res.json({ token });
   });
+
+  router.post('/verify-token', (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        res.json({decoded})
+    } catch (error) {
+        res.json(401).json({errpr: 'Invalid token'})
+    }
+    
+  });
+  
 module.exports = router;
