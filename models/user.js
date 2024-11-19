c812335cfb7083cfb28a9e6428de6cdd397157b5
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-11-19 09:27:11 -0500

    User updated

--- models/user.js
+++ models/user.js
@@ -3,10 +3,13 @@ const {Schema, default: mongoose} = require('mongoose')
 const userSchema = new Schema(
     {
         username: {type: String, required: true},
-        hashedPassword: {type: String, required: true}
-    
+        hashedPassword: {type: String, required: true},
+        mentorStatus: {type: Boolean, required: false},
+        fitnessPoints: {type: Number, min: 0, max: 20, required: false},
+        videoGamesPoints: {type: Number, min: 0, max: 20, required: false},
+        boardGamesPoints: {type: Number, min: 0, max: 20, required: false},
+        musicInsPoints: {type: Number, min: 0, max: 20, required: false}
     },
-
 
     {timestamps : true},
 )
