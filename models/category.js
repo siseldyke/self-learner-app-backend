1e12c4d6a9515571e2e9739481d7869c11f5f00b
siseldyke <siseldyke@gmail.com>
2024-11-19 09:13:57 -0500

    category scheme fully set up

--- models/category.js
+++ models/category.js
@@ -3,6 +3,10 @@ const {Schema, default: mongoose} = require('mongoose')
 const categorySchema = new Schema(
     {
         name: {type: String, required: true},
+        mentorConnection: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: false },
+        questionarre: {type: mongoose.Schema.Types.ObjectId, ref: 'questionare', required: false},
+        event: {type: mongoose.Schema.Types.ObjectId, ref: 'event', required: false},
+        businessess: {type: mongoose.Schema.Types.ObjectId, ref: 'user', required: false},
         link: {type: String, required: true}
     
     },
