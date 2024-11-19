ff3e0cda6955a2fbc198ffa132661699630afeb6
XOKEESH <hello@kishamartin.com>
2024-11-19 08:06:47 -0800

    Added post to controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -26,4 +26,30 @@ router.get('/:categoryId', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const { name, link, mentorConnection, questionarre, event, businessess } = req.body
+
+        if (!name || !link) {
+            return res.status(400).json({ error: 'Name and Link are required' })
+        }
+
+        const category = new Category({
+            name,
+            link,
+            mentorConnection,
+            questionarre,
+            event,
+            businessess
+        })
+
+        await category.save()
+
+        res.status(201).json({ message: 'New category created successfully', category })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
+})
+
 module.exports = router
