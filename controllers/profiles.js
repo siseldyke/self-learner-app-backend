b839400350d9558289c319ce3c9b6f892c0addc1
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-11-20 10:45:09 -0500

    User schema model and functionality has been updated

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const User = require('../models/user');
 const verifyToken = require('../middleware/verify-token')
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
 router.get('/:userId', verifyToken, async (req, res) => {
     try {
       if (req.user._id !== req.params.userId){ 
@@ -22,5 +31,31 @@ router.get('/:userId', verifyToken, async (req, res) => {
       }
     }
   });
+
+router.put('/:userId', verifyToken, async (req, res) => {
+  try {
+    if (req.user._id !== req.params.userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      res.status(404);
+      throw new Error('Profile not found.');
+    }
+
+    const { mentorStatus, fitnessPoints, videoGamesPoints, boardGamesPoints, musicInsPoints, isNewAccount } = req.body; 
+    if (mentorStatus !== undefined) user.mentorStatus = mentorStatus; 
+    if (fitnessPoints !== undefined) user.fitnessPoints = fitnessPoints; 
+    if (videoGamesPoints !== undefined) user.videoGamesPoints = videoGamesPoints; 
+    if (boardGamesPoints !== undefined) user.boardGamesPoints = boardGamesPoints; 
+    if (musicInsPoints !== undefined) user.musicInsPoints = musicInsPoints; 
+    if (isNewAccount !== undefined) user.isNewAccount = isNewAccount;
+
+    await user.save();
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
--- models/user.js
+++ models/user.js
@@ -5,10 +5,11 @@ const userSchema = new Schema(
         username: {type: String, required: true},
         hashedPassword: {type: String, required: true},
         mentorStatus: {type: Boolean, required: false},
-        fitnessPoints: {type: Number, min: 0, max: 20, required: false},
-        videoGamesPoints: {type: Number, min: 0, max: 20, required: false},
-        boardGamesPoints: {type: Number, min: 0, max: 20, required: false},
-        musicInsPoints: {type: Number, min: 0, max: 20, required: false}
+        fitnessPoints: {type: Number, default: 0, min: 0, max: 20, required: false},
+        videoGamesPoints: {type: Number, default: 0, min: 0, max: 20, required: false},
+        boardGamesPoints: {type: Number, default: 0, min: 0, max: 20, required: false},
+        musicInsPoints: {type: Number, default: 0, min: 0, max: 20, required: false},
+        isNewAccount: {type: Boolean, default: true, required: true}
     },
 
     {timestamps : true},
